e3b23678f6b10447e38d1185f70461a2ed840eab
Joao Humberto <joaohbmenezes@outlook.com>
2022-03-13 21:11:26 -0300

    feature: category form

--- routes/admin.js
+++ routes/admin.js
@@ -11,11 +11,11 @@ router.get('/posts', (req, res) =>{
 })
 
 router.get('/categorias', (req, res) =>{
-    res.render('/admin/categorias')
+    res.render('./admin/categorias')
 })
 
 router.get('/categorias/add', (req, res) =>{
-    res.render('/admin/addcategorias')
+    res.render('./admin/addcategorias')
 })
 
 
